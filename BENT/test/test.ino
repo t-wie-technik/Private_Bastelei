c635783bdf6df2f322bff993b1d146b5e1749623
TitusSchulz <pc.titus.schulz@gmail.com>
2021-05-01 20:55:05 +0200

    comit

--- BENT/test/test.ino
+++ BENT/test/test.ino
@@ -22,19 +22,8 @@
   #include <ESPAsyncWebServer.h>
   #include <FS.h>
 #endif
-#include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BME280.h>
 
-/*#include <SPI.h>
-#define BME_SCK 18
-#define BME_MISO 19
-#define BME_MOSI 23
-#define BME_CS 5*/
 
-Adafruit_BME280 bme; // I2C
-//Adafruit_BME280 bme(BME_CS); // hardware SPI
-//Adafruit_BME280 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK); // software SPI
 
 // Replace with your network credentials
 const char* ssid = "REPLACE_WITH_YOUR_SSID";
@@ -43,44 +32,6 @@ const char* password = "REPLACE_WITH_YOUR_PASSWORD";
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
 
-String readBME280Temperature() {
-  // Read temperature as Celsius (the default)
-  float t = bme.readTemperature();
-  // Convert temperature to Fahrenheit
-  //t = 1.8 * t + 32;
-  if (isnan(t)) {    
-    Serial.println("Failed to read from BME280 sensor!");
-    return "";
-  }
-  else {
-    Serial.println(t);
-    return String(t);
-  }
-}
-
-String readBME280Humidity() {
-  float h = bme.readHumidity();
-  if (isnan(h)) {
-    Serial.println("Failed to read from BME280 sensor!");
-    return "";
-  }
-  else {
-    Serial.println(h);
-    return String(h);
-  }
-}
-
-String readBME280Pressure() {
-  float p = bme.readPressure() / 100.0F;
-  if (isnan(p)) {
-    Serial.println("Failed to read from BME280 sensor!");
-    return "";
-  }
-  else {
-    Serial.println(p);
-    return String(p);
-  }
-}
 
 void setup(){
   // Serial port for debugging purposes
@@ -89,11 +40,7 @@ void setup(){
   bool status; 
   // default settings
   // (you can also pass in a Wire library object like &Wire2)
-  status = bme.begin(0x76);  
-  if (!status) {
-    Serial.println("Could not find a valid BME280 sensor, check wiring!");
-    while (1);
-  }
+  
 
   // Initialize SPIFFS
   if(!SPIFFS.begin()){
